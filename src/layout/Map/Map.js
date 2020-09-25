e628edc21d9bcd9ab2c5ec57c2ec1d1eee49bd6d
Anne-Gaisne <anne.gaisne@gmail.com>
2020-09-25 12:50:56 +0200

    Added color to the country depending on a rank
    For now the rank is just the legnth of the country name.
    It is computed in the getRank method that will be linked to the API.

--- src/layout/Map/Map.js
+++ src/layout/Map/Map.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import { Map, TileLayer } from 'react-leaflet';
+import Choropleth from 'react-leaflet-choropleth';
 
 import 'leaflet/dist/leaflet.css';
 import './Map.css';
+
+import topology from '../../context/countries-50m.json';
+
+const style = {
+  fillColor: '#F28F3B',
+  weight: 2,
+  opacity: 1,
+  color: 'white',
+  dashArray: '3',
+  fillOpacity: 0.5
+};
+
+function getRank(feature) {
+  return feature.properties.name.length;
+}
 
 function MapContainer() {
   return (
@@ -10,6 +26,15 @@ function MapContainer() {
       <TileLayer
         url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Choropleth
+        data={topology}
+        valueProperty={(feature) => getRank(feature)}
+        scale={['#b3cde0', '#011f4b']}
+        steps={7}
+        mode="e"
+        style={style}
+        onEachFeature={(feature, layer) => layer.bindPopup(feature.properties.name)}
       />
     </Map>
   );
