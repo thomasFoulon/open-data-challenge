b46bffd7710cecb0fe9074891bb6ff4f943baab4
Thomas Foulon <dev.thomas.foulon@gmail.com>
2020-09-25 15:27:15 +0200

    Add select and deselect all option on country selection
    - A more practical way to add countries on the chart,
    - Refactor to use functional components instead of classed components.

--- src/layout/ChartBoard/ChartBoard.js
+++ src/layout/ChartBoard/ChartBoard.js
@@ -1,71 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import './ChartBoard.css';
 import CountriesSelection from './CountriesSelection';
 import BarPlot from './BarPlot';
 
-class ChartBoard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedCountries: new Set()
-    };
-    this.allCountries = [
-      {
-        id: 'a', name: 'France', gdp: 351, transport: 34
-      },
-      {
-        id: 'b', name: 'Etats-Unis', gdp: 1542, transport: 56
-      },
-      {
-        id: 'c', name: 'Maroc', gdp: 657, transport: 20
-      },
-      {
-        id: 'd', name: 'Argentine', gdp: 210, transport: 86
-      },
-      {
-        id: 'e', name: 'Portugal', gdp: 124, transport: 200
-      },
-      {
-        id: 'f', name: 'Chili', gdp: 40, transport: 25
-      },
-      {
-        id: 'g', name: 'Chine', gdp: 1309, transport: 78
-      },
-      {
-        id: 'h', name: 'Japon', gdp: 32, transport: 3
-      }
-    ];
-    this.toogleCountry = this.toogleCountry.bind(this);
-  }
-
-  toogleCountry(toogledCountry) {
-    const { selectedCountries } = this.state;
-    if (selectedCountries.has(toogledCountry.id)) {
-      selectedCountries.delete(toogledCountry.id);
-    } else {
-      selectedCountries.add(toogledCountry.id);
+function ChartBoard() {
+  const [selectedCountryIds, setSelectedCountryIds] = useState(new Set());
+  const countries = [
+    {
+      id: 'a', name: 'France', gdp: 351, transport: 34
+    },
+    {
+      id: 'b', name: 'Etats-Unis', gdp: 1542, transport: 56
+    },
+    {
+      id: 'c', name: 'Maroc', gdp: 657, transport: 20
+    },
+    {
+      id: 'd', name: 'Argentine', gdp: 210, transport: 86
+    },
+    {
+      id: 'e', name: 'Portugal', gdp: 124, transport: 200
+    },
+    {
+      id: 'f', name: 'Chili', gdp: 40, transport: 25
+    },
+    {
+      id: 'g', name: 'Chine', gdp: 1309, transport: 78
+    },
+    {
+      id: 'h', name: 'Japon', gdp: 32, transport: 3
     }
-    this.setState({
-      selectedCountries
-    });
-  }
+  ];
 
-  render() {
-    const { selectedCountries } = this.state;
-    const countries = this.allCountries.filter((country) => selectedCountries.has(country.id));
-    return (
-      <div id="ChartBoard" className="ChartBoard">
-        <CountriesSelection
-          className="countriesSelection"
-          countries={this.allCountries}
-          selectedCountries={selectedCountries}
-          onChange={(toogledCountry) => this.toogleCountry(toogledCountry)}
-        />
-        <BarPlot countries={countries} />
-      </div>
-    );
-  }
+  const selectedCountries = countries.filter((country) => selectedCountryIds.has(country.id));
+  return (
+    <div id="ChartBoard" className="ChartBoard">
+      <CountriesSelection
+        className="countriesSelection"
+        countries={countries}
+        selectedCountryIds={selectedCountryIds}
+        onChange={(selectedIds) => setSelectedCountryIds(new Set(selectedIds))}
+      />
+      <BarPlot countries={selectedCountries} />
+    </div>
+  );
 }
 
 export default ChartBoard;
--- src/layout/ChartBoard/CountriesSelection.js
+++ src/layout/ChartBoard/CountriesSelection.js
@@ -1,45 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class CountriesSelection extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchValue: '',
-      displayedCountries: props.countries
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
+function CountriesSelection(props) {
+  const { countries, selectedCountryIds, onChange } = props;
 
-  handleChange(event) {
-    const { countries } = this.props;
+  const [searchValue, setSearchValue] = useState('');
+  const [displayedCountries, setDisplayedCountries] = useState(countries);
+
+  const searchCountries = (event) => {
     const newSearchValue = event.target.value.trim().toLowerCase();
     const newDisplayedCountries = countries.filter(
       (item) => item.name.trim().toLowerCase().substr(0, newSearchValue.length)
         === newSearchValue
     );
-    this.setState({
-      searchValue: newSearchValue,
-      displayedCountries: newDisplayedCountries
-    });
-  }
+    setSearchValue(newSearchValue);
+    setDisplayedCountries(newDisplayedCountries);
+  };
 
-  render() {
-    const { searchValue, displayedCountries } = this.state;
-    const { selectedCountries, onChange } = this.props;
-    return (
-      <div className="contriesSelection">
-        <input type="text" value={searchValue} onChange={this.handleChange} />
-        <ul>
-          {displayedCountries.map((country) => (
-            <li key={country.id}>
-              <input type="checkbox" defaultChecked={selectedCountries.has(country.id)} onClick={() => onChange(country)} />
-              {country.name}
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+  const toggleCountry = (countryId) => {
+    if (selectedCountryIds.has(countryId)) {
+      selectedCountryIds.delete(countryId);
+    } else {
+      selectedCountryIds.add(countryId);
+    }
+    onChange(Array.from(selectedCountryIds));
+  };
+
+  return (
+    <div className="contriesSelection">
+      <input type="text" value={searchValue} onChange={searchCountries} />
+      <ul>
+        {displayedCountries.map((country) => (
+          <li key={country.id}>
+            <input
+              type="checkbox"
+              checked={selectedCountryIds.has(country.id)}
+              onChange={() => toggleCountry(country.id)}
+            />
+            {country.name}
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => onChange(countries.map((country) => country.id))}>
+        Tout sélectionner
+      </button>
+      <button onClick={() => onChange([])}>Tout déselectionner</button>
+    </div>
+  );
 }
 
 export default CountriesSelection;
