1039764a87998e2c245531184d4e36948948cbab
elmouradiAmine <elmouradi.amine98@gmail.com>
2020-09-25 10:12:50 +0200

    add header navigation

---
+++ src/layout/Report/Report.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Report.css';
+
+function Report() {
+  return (
+    <div id="Report" className="Report">
+      Report
+    </div>
+  );
+}
+
+export default Report;
