878c80cf88878a65af8b860347820d8348bfbe2a
Anne Gaisne <anne.gaisne@gmail.com>
2020-09-25 18:37:46 +0200

    Color map (#26)
    * Added color to the country depending on a rank
    
    For now the rank is just the legnth of the country name.
    It is computed in the getRank method that will be linked to the API.
    
    * Added a tooltip to show info about the hovered countries
    
    Show the name and the rank of the country hovered by the mouse on the map.
    Note that for now the rank is a placeholder data and must be linked to
    real data
    
    * Added map bound to prevent weird display
    
    Control the zoom to be able to not zoom too much as it is not necessary
    Prevent from zooming out too much
    Prevent panning out of the map area

--- src/layout/Map/Map.js
+++ src/layout/Map/Map.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import { Map, TileLayer } from 'react-leaflet';
+import Choropleth from 'react-leaflet-choropleth';
 
 import 'leaflet/dist/leaflet.css';
 import './Map.css';
+
+import topology from '../../context/countries-50m.json';
+
+const style = {
+  fillColor: '#F28F3B',
+  weight: 1,
+  color: 'grey',
+  fillOpacity: 0.5
+};
+
+const mapBounds = [[89, 179], [-89, -179]];
+
+function getRank(feature) {
+  return feature.properties.name.length;
+}
+
+function setEventOnEachFeature(feature, layer) {
+  layer.bindTooltip(`${feature.properties.name} <br/> ${getRank(feature)}`);
+  layer.on('mouseover', (e) => {
+    e.target.openTooltip(e.latlng);
+  });
+  layer.on('mouseout', (e) => {
+    e.target.closeTooltip();
+  });
+  layer.on('mousemove', (e) => {
+    e.target.getTooltip().setLatLng(e.latlng);
+  });
+}
 
 function MapContainer() {
   return (
@@ -10,6 +39,17 @@ function MapContainer() {
       <TileLayer
         url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        noWrap
+        bounds={mapBounds}
+      />
+      <Choropleth
+        data={topology}
+        valueProperty={(feature) => getRank(feature)}
+        scale={['#b3cde0', '#011f4b']}
+        steps={7}
+        mode="e"
+        style={style}
+        onEachFeature={(feature, layer) => { setEventOnEachFeature(feature, layer); }}
       />
     </Map>
   );
