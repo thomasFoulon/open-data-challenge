8c5db1a1b120a0b4e356302db1aafc06981e4163
elmouradiAmine <elmouradi.amine98@gmail.com>
2020-09-26 22:07:41 +0200

    add footer

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
+import ensimagLogo from '../../assets/ensimag.png';
+import gapminderLogo from '../../assets/gapminder.png';
+import worldBankLogo from '../../assets/world-bank.png';
+
 import './Footer.css';
 
 function Footer() {
-  return <div className="Footer">Footer</div>;
+  return (
+    <div className="Footer">
+      &copy; Copyrights 2020. Réalisé par ELMOURADI Amine, ELMOURADI Imane, FOULON Thomas, GAISNE
+      Anne et ALLALI Oussama.
+      <div className="Footer__links">
+        <a href="https://ensimag.grenoble-inp.fr/" target="_blank" rel="noreferrer">
+          <img className="Footer__image" src={ensimagLogo} alt="" />
+
+        </a>
+
+        <a href="https://data.worldbank.org/" target="_blank" rel="noreferrer">
+          <img className="Footer__image" src={worldBankLogo} alt="" />
+
+        </a>
+
+        <a href="https://www.gapminder.org/" target="_blank" rel="noreferrer">
+          <img className="Footer__image" src={gapminderLogo} alt="" />
+
+        </a>
+      </div>
+
+    </div>
+  );
 }
 
 export default Footer;
