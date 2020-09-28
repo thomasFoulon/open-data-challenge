6da3eee1936a8aa1ee364e4bebc196402faebc6e
thomasFoulon <67117547+thomasFoulon@users.noreply.github.com>
2020-09-28 18:17:16 +0200

    Improve countries comparison user interface (#39)
    - Delete the pink background,
    - Change the search bar by a dropdown list,
    - Change the style of the items,
    - Change the style of the buttons,
    - Fix the ChartBoard height according to the screen height,
    - Align on the same line the two selectors (country and indicator),
    - Put the chart under the selectors.
    - Set selection buttons on the right of the dropdown list

--- src/layout/ChartBoard/ChartBoard.js
+++ src/layout/ChartBoard/ChartBoard.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 
+import { Chart } from 'react-chartjs-2';
+
 import './ChartBoard.css';
 import CountriesSelection from './CountriesSelection';
 import BarPlot from './BarPlot';
 
 function ChartBoard() {
   const [selectedCountryIds, setSelectedCountryIds] = useState(new Set());
+  const [currentDatasetId, setCurrentDatasetId] = useState(0);
+  const { color } = Chart.helpers;
   const countries = [
     {
       id: 'a', name: 'France', gdp: 351, transport: 34
@@ -30,19 +34,71 @@ function ChartBoard() {
     },
     {
       id: 'h', name: 'Japon', gdp: 32, transport: 3
+    },
+    {
+      id: 'i', name: 'Russie', gdp: 351, transport: 34
+    },
+    {
+      id: 'j', name: 'Egypte', gdp: 1542, transport: 56
+    },
+    {
+      id: 'k', name: 'Inde', gdp: 657, transport: 20
+    },
+    {
+      id: 'l', name: 'Pérou', gdp: 210, transport: 86
+    },
+    {
+      id: 'm', name: 'Vénézuela', gdp: 124, transport: 200
+    },
+    {
+      id: 'n', name: 'Ethiopie', gdp: 40, transport: 25
+    },
+    {
+      id: 'o', name: 'Belgique', gdp: 1309, transport: 78
+    },
+    {
+      id: 'p', name: 'Luxembourg', gdp: 32, transport: 3
     }
   ];
+
+  const datasets = [{
+    label: 'PIB/hab',
+    property: 'gdp',
+    backgroundColor: color('red').alpha(0.5).rgbString(),
+    borderColor: 'red',
+    borderWidth: 1
+  }, {
+    label: 'Qualité des transports',
+    property: 'transport',
+    backgroundColor: color('blue').alpha(0.5).rgbString(),
+    borderColor: 'blue',
+    borderWidth: 1
+  }];
+
+  const handleChange = (event) => {
+    setCurrentDatasetId(event.target.value);
+  };
 
   const selectedCountries = countries.filter((country) => selectedCountryIds.has(country.id));
   return (
     <div id="ChartBoard" className="ChartBoard">
-      <CountriesSelection
-        className="countriesSelection"
-        countries={countries}
-        selectedCountryIds={selectedCountryIds}
-        onChange={(selectedIds) => setSelectedCountryIds(new Set(selectedIds))}
-      />
-      <BarPlot countries={selectedCountries} />
+      <div className="selection">
+        <CountriesSelection
+          countries={countries}
+          selectedCountryIds={selectedCountryIds}
+          onChange={(selectedIds) => setSelectedCountryIds(new Set(selectedIds))}
+        />
+        <div className="indicatorSelector">
+          <select value={currentDatasetId} onChange={handleChange}>
+            {datasets.map(
+              (dataset, index) => (
+                <option key={dataset.label} value={index}>{dataset.label}</option>
+              )
+            )}
+          </select>
+        </div>
+      </div>
+      <BarPlot datasets={datasets} datasetId={currentDatasetId} countries={selectedCountries} />
     </div>
   );
 }
