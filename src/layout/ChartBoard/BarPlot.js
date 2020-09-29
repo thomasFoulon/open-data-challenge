7f0a81ebaf0a35ec6e81e5c6e340568e8b9851e5
Thomas Foulon <dev.thomas.foulon@gmail.com>
2020-09-29 11:58:30 +0200

    Link the data to the chart board
    - Add a desc attribute on each indicator to indicate in which way countries have to be sorted according to the indicator,
    - Display only the first 50 countries when a lot of countries are selected.

--- src/layout/ChartBoard/BarPlot.js
+++ src/layout/ChartBoard/BarPlot.js
@@ -1,33 +1,66 @@
 import React from 'react';
 
-import { HorizontalBar } from 'react-chartjs-2';
+import { Chart, HorizontalBar } from 'react-chartjs-2';
+
+const { color } = Chart.helpers;
+const colors = ['#FFFF00', '#1CE6FF', '#FF34FF', '#FF4A46', '#008941', '#006FA6', '#A30059', '#FFDBE5'];
 
 export default function BarPlot(props) {
   const {
-    countries,
-    datasets,
-    datasetId
+    selectedCountries,
+    indicators,
+    currentIndicatorId
   } = props;
 
-  if (countries.length === 0) {
+  const datasets = indicators.map((indicator, index) => {
+    const datasetColor = colors[index % colors.length];
+    return {
+      label: indicator.content,
+      backgroundColor: color(datasetColor).alpha(0.5).rgbString(),
+      borderColor: datasetColor,
+      borderWidth: 1
+    };
+  });
+
+  if (selectedCountries.length === 0) {
     return (
       <p className="chart">Sélectionnez un pays.</p>
     );
   }
 
-  const sortedCountries = countries.sort(
-    (country1, country2) => country1[datasets[datasetId].property]
-      < country2[datasets[datasetId].property]
+  const { id, desc } = indicators[currentIndicatorId];
+
+  const filteredCountries = selectedCountries.filter(
+    (country) => country[id] !== undefined && country[id] !== null
   );
 
-  datasets[datasetId].data = sortedCountries.map(
-    (country) => country[datasets[datasetId].property]
-  );
+  const compareDesc = (country1, country2) => country1[id] < country2[id];
+  const compareAsc = (country1, country2) => country1[id] > country2[id];
+
+  const sortedCountries = filteredCountries.sort((country1, country2) => {
+    if (desc) return compareDesc(country1, country2);
+    return compareAsc(country1, country2);
+  });
+
+  const top50 = sortedCountries.slice(0, 49);
+
+  datasets[currentIndicatorId].data = top50.map((country) => country[id]);
 
   const barChartData = {
-    labels: sortedCountries.map((country) => country.name),
-    datasets: [datasets[datasetId]]
+    labels: top50.map((country) => country.name),
+    datasets: [datasets[currentIndicatorId]]
   };
+
+  let title = 'Comparatif des indicateurs';
+
+  if (sortedCountries.length > 50) {
+    title += ' (50 meilleurs affichés)';
+  }
+
+  if (filteredCountries.length < selectedCountries.length) {
+    const diff = selectedCountries.length - filteredCountries.length;
+    title += ` (${diff} sans données)`;
+  }
 
   return (
     <div className="chart">
@@ -41,7 +74,7 @@ export default function BarPlot(props) {
           },
           title: {
             display: true,
-            text: 'Comparatif des indicateurs'
+            text: title
           },
           scales: {
             xAxes: [{
