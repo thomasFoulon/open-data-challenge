b730faf801f77f65e685096e24882df8274b693a
Anne Gaisne <anne.gaisne@gmail.com>
2020-09-30 15:04:50 +0200

    Better score (#68)
    * Changed score computation with index
    
    Delete some country on the choropleth that had no data linked:
     - Somaliland (linked now to Somalie)
     - Western Saharah (linked now to Morocco)
     - French Guiana (linked now to France)
     - Falkland Island
     - Taiwan
     - Antartica
    
    * Did the mean of all dimension indicator for the score
    
    Fixed the zoom on the map
    
    * Changed Map background and opacity of color
    
    Also removed territory with no data
    
    * Moved file country-50m.json to the assets directory for coherance
    
    * Used indicators length to compute the mean
    
    Instead of the hardcoded value 7.
    This will be usefull if indicators are added to the list.

--- src/layout/Map/Map.js
+++ src/layout/Map/Map.js
@@ -5,13 +5,13 @@ import Choropleth from 'react-leaflet-choropleth';
 import 'leaflet/dist/leaflet.css';
 import './Map.css';
 
-import topology from '../../context/countries-50m.json';
+import topology from '../../assets/json/countries-50m.json';
 
 const style = {
   fillColor: '#F28F3B',
   weight: 1,
-  color: 'grey',
-  fillOpacity: 0.5,
+  color: 'darkgrey',
+  fillOpacity: 0.7,
 };
 
 const mapBounds = [
@@ -57,8 +57,8 @@ function MapContainer(props) {
       maxBounds={mapBounds}
     >
       <TileLayer
-        url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Shaded_Relief/MapServer/tile/{z}/{y}/{x}"
+        attribution="Tiles &copy; Esri &mdash; Source: Esri"
         noWrap
         bounds={mapBounds}
       />
@@ -70,7 +70,7 @@ function MapContainer(props) {
         mode="e"
         style={style}
         onEachFeature={(country, layer) => {
-          setEventOnEachFeature(country, layer, scores);
+          setEventOnEachFeature(country, layer);
         }}
       />
     </Map>
