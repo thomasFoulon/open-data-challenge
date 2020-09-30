a54eb0c2b0c336590b7c5f9217ca389eb5c2efc0
El Mouradi Imane <asphedri@gmail.com>
2020-09-30 20:55:46 +0200

    fix(sidebar): hide social media icon if there's no link (#86)

--- src/components/Author/Author.js
+++ src/components/Author/Author.js
@@ -18,26 +18,34 @@ function Author({
         <p className="Author__name">{name}</p>
         {isTeacher ? <p className="enseignant "> Enseignant</p> : (
           <ul className="Author__social-links">
-            <li className="social__item">
-              <a className="social__link" href={github} target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={faGithub} size="lg" />
-              </a>
-            </li>
-            <li className="social__item">
-              <a className="social__link" href={linkedin} target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={faLinkedin} size="lg" />
-              </a>
-            </li>
-            <li className="social__item">
-              <a className="social__link" href={facebook} target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={faFacebook} size="lg" />
-              </a>
-            </li>
-            <li className="social__item">
-              <a className="social__link" href={twitter} target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={faTwitter} size="lg" />
-              </a>
-            </li>
+            {github && (
+              <li className="social__item">
+                <a className="social__link" href={github} target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={faGithub} size="lg" />
+                </a>
+              </li>
+            )}
+            { linkedin && (
+              <li className="social__item">
+                <a className="social__link" href={linkedin} target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={faLinkedin} size="lg" />
+                </a>
+              </li>
+            )}
+            {facebook && (
+              <li className="social__item">
+                <a className="social__link" href={facebook} target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={faFacebook} size="lg" />
+                </a>
+              </li>
+            )}
+            {twitter && (
+              <li className="social__item">
+                <a className="social__link" href={twitter} target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={faTwitter} size="lg" />
+                </a>
+              </li>
+            )}
           </ul>
         )}
 
